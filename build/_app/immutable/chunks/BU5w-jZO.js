a292b2b4c820bd4cb957a03cf0e76c5655a8818a
Richard Pickup <rich@richyp.com>
2025-05-09 09:48:30 +0100

    build folder

---
+++ build/_app/immutable/chunks/BU5w-jZO.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1mm6bh3)==null?void 0:s.base)??"/richyp-com";var a;const t=((a=globalThis.__sveltekit_1mm6bh3)==null?void 0:a.assets)??e;export{t as a,e as b};
---
+++ build/_app/immutable/chunks/TT-aNd2k.js
@@ -0,0 +1,1 @@
+import{e}from"./CTpV-J2g.js";e();
