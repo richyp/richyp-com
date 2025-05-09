7bdcebd1a296dd07e02fc5e949a67fbffdbedb32
Richard Pickup <rich@richyp.com>
2025-05-09 10:08:03 +0100

    Tailwind

--- build/_app/immutable/nodes/3.DzcJx09X.js
+++
@@ -1,2 +0,0 @@
-import{t as o,a as s}from"../chunks/CAkoqTEI.js";import"../chunks/TT-aNd2k.js";import{s as e,f as m,n as i}from"../chunks/CTpV-J2g.js";import{s as p}from"../chunks/DovdUOYa.js";import{b as f}from"../chunks/ggrIAR-V.js";var n=o(`About
----- <a>Home</a>`,1);function v(t){i();var a=n(),r=e(m(a));p(r,"href",`${f??""}/`),s(t,a)}export{v as component};
