898614e8132ba6b842e8c1ae467dcf079d094616
Richard Pickup <rich@richyp.com>
2025-05-09 10:00:10 +0100

    Updates

---
+++ _app/immutable/nodes/3.D9RQBfBV.js
@@ -0,0 +1,2 @@
+import{t as o,a as s}from"../chunks/CAkoqTEI.js";import"../chunks/TT-aNd2k.js";import{s as e,f as m,n as i}from"../chunks/CTpV-J2g.js";import{s as p}from"../chunks/DovdUOYa.js";import{b as f}from"../chunks/kPH0LEiH.js";var n=o(`About
+---- <a>Home</a>`,1);function v(t){i();var a=n(),r=e(m(a));p(r,"href",`${f??""}/`),s(t,a)}export{v as component};
